4657aa43bd5bd2cab5446845c7f6844578dfbcbc
ljx360 <79862310+ljx360@users.noreply.github.com>
2024-12-07 22:06:51 +0800

    Update ljx.sh

--- ljx.sh
+++ ljx.sh
@@ -1,7 +1,7 @@
 
 #ip
-sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_generate
-
+#sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_generate
+#do in diy-part2.sh
 
 #package
 git clone https://github.com/kenzok8/small-package.git package/small-package
